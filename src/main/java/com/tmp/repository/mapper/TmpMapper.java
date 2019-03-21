ce41e5165223fbed83fbce105d3e6dbace6134ce
shiguowen <guowen_shi@wishbuild.com>
2019-03-21 09:54:48 +0800

    init

---
+++ src/main/java/com/tmp/repository/mapper/TmpMapper.java
@@ -0,0 +1,13 @@
+package com.tmp.repository.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.tmp.mybatis.MyMapper;
+import com.tmp.repository.po.Tmp;
+
+@Mapper
+public interface TmpMapper extends MyMapper<Tmp> {
+
+	
+
+}
---
+++ src/main/java/com/tmp/repository/po/Tmp.java
@@ -0,0 +1,13 @@
+package com.tmp.repository.po;
+
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+public class Tmp {
+
+	@Id
+	private String id;
+	
+}
